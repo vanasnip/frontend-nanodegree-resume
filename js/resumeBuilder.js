9bcc1298bab940420d93d0f7b17dc6abe357cd4a
Ivan Mamenemuno <IMamenemuno@ArkGroup.local>
2016-08-22 17:26:50 +0100

    Pending sorting input name and surname capitalisation

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,68 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+// var name = "Ivan Mamenemuno";
+// var formattedName = HTMLheaderName.replace("%data%", name);
+//
+//
+// var role = "Front-End Web Developer";
+// var formattedRole = HTMLheaderRole.replace("%data%", role);
+//
+//
+//
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
+
+/******************************************************************************/
+// var skills = ["Awesomeness", "Programming", "Design", "Github"];
+// $("#main").append(skills.length);
+
+/******************************************************************************/
+
+
+// var sampleArray = [0,0,7, 7, 5];
+//
+// var incrementLastArrayElement = function(_array) {
+//     var newArray = [];
+//     // Your code should make newArray equal to an array that has the same
+//     // values as _array, but the last number has increased by one.
+//
+//     // For example:
+//     // _array = [1, 2, 3];
+//     // turns into:
+//
+//     // Your code goes in here!
+//
+//     var lastNo = _array.pop();
+//     _array[_array.length] = lastNo + 1;
+//     newArray = _array;
+//
+//
+//     // Don't delete this line!
+//     return newArray;
+// };
+//
+// // Did your code work? The line below will tell you!
+// console.log(incrementLastArrayElement(sampleArray));
+
+/******************************************************************************/
+
+var name = "AlbERt EINstEiN";
+
+function nameChanger(oldName) {
+    var finalName = oldName;
+    // Your code goes here!
+    var space = oldName.indexOf(" ");
+  var surname =  oldName.slice(space+1);
+  finalName = oldName.pop(space);
+  console.log(space + " space");
+
+
+
+    // Don't delete this line!
+    return finalName;
+}
+
+// Did your code work? The line below will tell you!
+console.log(nameChanger(name));
