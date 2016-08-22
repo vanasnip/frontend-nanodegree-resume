b704a40c37f88d0a7b01e8199ae77f812dc6642e
vanasnip <ivan.mamenemuno@gmail.com>
2016-08-22 23:26:28 +0100

    Last entry is dot and bracket notation objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,6 +2,7 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
+/******************************************************************************/
 // var name = "Ivan Mamenemuno";
 // var formattedName = HTMLheaderName.replace("%data%", name);
 //
@@ -47,22 +48,50 @@ This is empty on purpose! Your code to build the resume will go here.
 // console.log(incrementLastArrayElement(sampleArray));
 
 /******************************************************************************/
+// var name = "ALBERT EINstEiN";
+//
+// function nameChanger(oldName) {
+//     var finalName = oldName;
+//     // Your code goes here!
+//     var space = oldName.indexOf(" ");
+//     var surname =  oldName.slice(space+1);
+//     var firstName = oldName.slice(0, -surname.length -1);
+//     console.log(space + " space");
+//     finalName = firstName[0].toUpperCase() + firstName.slice(1, firstName.length).toLowerCase() + " " + surname.toUpperCase();
+//
+//
+//     // Don't delete this line!
+//     return finalName;
+// }
+//
+// // Did your code work? The line below will tell you!
+// console.log(nameChanger(name));
+/******************************************************************************/
+var skills = ["HTML5", "CSS3", "Bootstrap", "Javascript", "Photoshop", "Illustrator", "Inkscape", "Grunt", "git", "GitHub", "Udacity Graduate" ];
+var bio = {
+  "name" : "Ivan Mamenemuno",
+  "role" : "Web Developer",
+  "contacts" : {
+    "email" : "ivan.mamenemuno@gmail.com",
+    "location" : "Wixams, bedforshire"
+  },
+  "skills" : skills,
+};
+bio.bioPic = "images/ivan.png";
+bio.contacts.github = "vanasnip";
 
-var name = "AlbERt EINstEiN";
+$("#main").append(bio.name);
+$("#main").append(bio.role);
+$("#main").append(bio.skills);
 
-function nameChanger(oldName) {
-    var finalName = oldName;
-    // Your code goes here!
-    var space = oldName.indexOf(" ");
-  var surname =  oldName.slice(space+1);
-  finalName = oldName.pop(space);
-  console.log(space + " space");
+var work = {};
+work.position = "Sale and Marketing Executive";
+work.employer = "Ark-H Handling";
+work.city = "Bedford";
 
+var education = {};
+education["college"] = "Barnfield College";
+education["years"] = "2007 - 2009";
 
-
-    // Don't delete this line!
-    return finalName;
-}
-
-// Did your code work? The line below will tell you!
-console.log(nameChanger(name));
+$("#main").append(work["position"]);
+$("#main").append(education.college);
