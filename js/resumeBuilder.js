d2ac19eee633d3a7ad057cac21f567574d9fffc1
Ivan Mamenemuno <IMamenemuno@ArkGroup.local>
2016-08-23 11:32:07 +0100

    Add course correct object names properties and methods for resumeBuilder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,7 +3,7 @@ var bio = {
   "name": "Ivan Mamenemuno",
   "role": "Front-End Web Developer",
   "welcomeMessage": "Welcome to my resume. I hope you see my credintials not just as merits, but as an opportunity and as an asset to your company, thank you",
-  "bioPic": "images/ivan.png",
+  "biopic": "images/ivan.png",
   "skills": ["HTML5", "CSS3", "Bootstrap", "Javascript", "Photoshop", "Illustrator", "Inkscape", "Grunt", "git", "GitHub", "Udacity Graduate" ],
   "contacts": {
     "mobile": "07746969638",
@@ -14,85 +14,77 @@ var bio = {
 };
 
 var work = {
-    "job": [
+    "jobs": [
     {
-      "title": "Sale and Marketing Executive",
       "employer": "Ark-H Handling",
-      "city": "Bedford",
+      "title": "Sale and Marketing Executive",
+      "location": "Bedford",
+      "dates": "9-2007 to Current Employer",
       "discription": "My role involved initially involved developing a new front-end for the website. A good opportuinity to implement contemporary web technologies and techniques.",
-      "startDate": [9,  2007],
-      "endDate": ["Current Employer"]
+
     },
     {
+      "employer": "Self Employed",
       "title": "Graphic Designer",
-      "employer": "Self Employed",
-      "city": "Luton",
+      "location": "Luton",
+      "dates": "9-2009 to 9-2015",
       "discription": "Working to client specification on flyers, posters, leaflets, brochures and other literature for print. As well as logo design and branding development for start-ups, I also worked on desgn for the web and email. ",
-      "startDate": [9,  2009],
-      "endDate": [4, 2015]
+
+
     },
     {
-      "title": "Photographer",
       "employer": "Self Employed",
-      "city": "Luton",
+      "title": "Photographer",
+      "location": "Luton",
+      "dates": "9-2009 to 9-2015",
       "discription": "My photography included portaits, coorperate events, weddings, parties, fashion and products. I worked on refferal and I developed a lot from what was just a hobby to begin with.",
-      "startDate": [9,  2009],
-      "endDate": [4, 2015]
     }
   ]
 };
 
 var projects = {
-  "project" : [
+  "projects" : [
     {
       "title": "Chess Styles",
+      "dates": "2-2016 to 4-2016",
       "discription": "users copy and paste algebraic chess notation for popular chess apps like chess.com. Chess styles will take this input and turn into a much more visually apealing, readable and easy to follow design layout. Web responsive and formatted for print",
-      "technologies": {
-        "tech": ["HTML", "CSS", "JavaScript"],
-        "techLogo": ["images/html.svg", "images/css.svg", "images/js.svg"]
-      },
       "images": ["images/proj1-01.png", "images/proj1-02.png", "images/proj1-03.png"]
     }
   ]
 };
 
 var education = {
-  "school": [
+  "schools": [
     {
-      "institute": "Barnfield College",
-      "city": "Luton, UK",
-      "level": "National Diploma",
-      "subject": "Information Technology",
-      "modules": ["Programming", "Networking", "Web Development", "Database"],
-      "startDate": [9,  2007],
-      "endDate": [7, 2009],
-      "acheived": "Distinction"
+      "name": "Barnfield College",
+      "location": "Luton, UK",
+      "degree": "National Diploma",
+      "majors": ["Information Technology"],
+      "date": "9-2007 to 7-2009",
+
     },
     {
-      "institute": "Coventry University",
-      "city": "Coventry, UK",
-      "level": "Foundation Degree",
-      "subject": "Art and Design",
-      "modules": ["Programming", "Networking", "Web Development", "Database"],
-      "startDate": [9,  2005],
-      "endDate": [7, 2006],
-      "achieved": "Merit"
+      "name": "Coventry University",
+      "location": "Coventry, UK",
+      "degree": "Foundation Degree",
+      "majors": ["Art and Design"],
+      "dates": "9-2005 to 7-2006",
     }
   ],
-  "onlineCourse": [
+  "onlineCourses": [
     {
       "title": "Front-End Developer Nanodegree",
-      "site": "Udacity.com",
-      "modules": ["Developer Mindset", "Responisive Web Design", "JQuery", "Object Oriented JavaScript", "HTML5 Canvas", "Web Performance Optimisation", "Browser Rendering Optimisation", "AJAX", "JavaScript Design Patterns"],
-      "startDate": [6, 2016],
-      "endDate": [11, 2016]
+      "school": "Udacity.com",
+      "dates": "6-2016 to 11-2016",
+      "url": "https://www.udacity.com/"
+
     },
     {
       "title": "Introduction to JavaScript",
-      "site": "Codecademy.com",
-      "modules": ["Functions", "Loops and Conditionals", "Data Structures", "Objects"],
-      "startDate": [8, 2015],
-      "endDate": [12, 2015]
+      "school": "Codecademy.com",
+      "dates": "8-2015 to 12-2015",
+      "url": "https://www.codecademy.com/"
+
     }
   ]
 };
