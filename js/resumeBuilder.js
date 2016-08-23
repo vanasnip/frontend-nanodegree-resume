9668fb99d8548cf0d39510770a4e8714a80448d0
vanasnip <ivan.mamenemuno@gmail.com>
2016-08-23 07:14:35 +0100

    Add 3 of 4 objects, 4th being projects object.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -67,31 +67,65 @@ This is empty on purpose! Your code to build the resume will go here.
 // // Did your code work? The line below will tell you!
 // console.log(nameChanger(name));
 /******************************************************************************/
-var skills = ["HTML5", "CSS3", "Bootstrap", "Javascript", "Photoshop", "Illustrator", "Inkscape", "Grunt", "git", "GitHub", "Udacity Graduate" ];
 var bio = {
-  "name" : "Ivan Mamenemuno",
-  "role" : "Web Developer",
-  "contacts" : {
-    "email" : "ivan.mamenemuno@gmail.com",
-    "location" : "Wixams, bedforshire"
+  "name": "Ivan Mamenemuno",
+  "role": "Web Developer",
+  "contacts": {
+    "email": "ivan.mamenemuno@gmail.com",
+    "github": "vanasnip",
+    "location": "Wixams, bedforshire"
   },
-  "skills" : skills,
+  "skills": ["HTML5", "CSS3", "Bootstrap", "Javascript", "Photoshop", "Illustrator", "Inkscape", "Grunt", "git", "GitHub", "Udacity Graduate" ],
+  "bioPic": "images/ivan.png"
 };
-bio.bioPic = "images/ivan.png";
-bio.contacts.github = "vanasnip";
 
-$("#main").append(bio.name);
-$("#main").append(bio.role);
-$("#main").append(bio.skills);
+
+
+
 
 var work = {};
 work.position = "Sale and Marketing Executive";
 work.employer = "Ark-H Handling";
 work.city = "Bedford";
 
-var education = {};
-education["college"] = "Barnfield College";
-education["years"] = "2007 - 2009";
 
-$("#main").append(work["position"]);
-$("#main").append(education.college);
+var education = {
+  "schools": [
+    {
+      "institute": "Barnfield College",
+      "city": "Luton, UK",
+      "level": "National Diploma",
+      "subject": "Information Technology",
+      "modules": ["Programming", "Networking", "Web Development", "Database"],
+      "startDate": [9,  2007],
+      "endDate": [7, 2009],
+      "acheived": "Distinction"
+    },
+    {
+      "institute": "Coventry University",
+      "city": "Coventry, UK",
+      "level": "Foundation Degree",
+      "subject": "Art and Design",
+      "modules": ["Programming", "Networking", "Web Development", "Database"],
+      "startDate": [9,  2005],
+      "endDate": [7, 2006],
+      "achieved": "Merit"
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title": "Front-End Developer Nanodegree",
+      "site": "Udacity.com",
+      "modules": ["Developer Mindset", "Responisive Web Design", "JQuery", "Object Oriented JavaScript", "HTML5 Canvas", "Web Performance Optimisation", "Browser Rendering Optimisation", "AJAX", "JavaScript Design Patterns"],
+      "startDate": [6, 2016],
+      "endDate": [11, 2016]
+    },
+    {
+      "title": "Introduction to JavaScript",
+      "site": "Codecademy.com",
+      "modules": ["Functions", "Loops and Conditionals", "Data Structures", "Objects"],
+      "startDate": [8, 2015],
+      "endDate": [12, 2015]
+    }
+  ]
+};
